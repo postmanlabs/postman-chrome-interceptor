0b3f4bdc1815c778544e2c68e0268cdf6d2369bf
Prakhar Srivastav <prakhar1989@gmail.com>
2014-02-25 16:24:43 +0300

    Added background_page integration for logger

--- extension/js/index_page.js
+++ extension/js/index_page.js
@@ -1,6 +1,23 @@
+// this port is available as soon as popup is opened
+var popupPort = chrome.runtime.connect({name: 'POPUPCHANNEL'});
+
+// DOM element for appending log messages
+var loggerList = document.getElementById('logger');
+
+// long-lived connection to the background channel 
 chrome.runtime.onConnect.addListener(function(port){
-  console.assert(port.name == "requestLogger");
-  port.onMessage.addListener(function(msg){
-    console.log(msg);
+  console.assert(port.name === 'BACKGROUNDCHANNEL');
+  console.log("Connected to background");
+
+  port.onMessage.addListener(function(msg) {
+    logRequest(msg);
   });
 });
+
+// manipulates DOM to add log msgs
+function logRequest(msg) {
+  var entry = document.createElement('li');
+  var textMsg = "[" + msg.method + "] " +  msg.url;
+  entry.appendChild(document.createTextNode(textMsg));
+  loggerList.appendChild(entry);
+}
