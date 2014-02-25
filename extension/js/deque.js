3cc6c74b02140e692432d42a2e4636be5b66887a
Prakhar Srivastav <prakhar1989@gmail.com>
2014-02-25 20:21:10 +0300

    changed deque api

--- extension/js/deque.js
+++ extension/js/deque.js
@@ -5,25 +5,19 @@
  * Methods: 
  * push(item) -> Adds an item in the queue
  * dequeue -> Pops the last item from the back and return it
- * getItems -> returns an array of items with most recently 
- * added item at the front
  */
 function Deque(N) {
   this._maxlength = N;
-  this._items = new Array();
+  this.items = new Array();
 }
 
 Deque.prototype.dequeue = function() {
-  return this._items.pop();
+  return this.items.pop();
 }
 
 Deque.prototype.push = function(item) {
-  this._items.unshift(item);
-  if (this._items.length > this._maxlength) {
+  this.items.unshift(item);
+  if (this.items.length > this._maxlength) {
     this.dequeue();
   }
-}
-
-Deque.prototype.getItems = function() {
-  return this._items;
 }
