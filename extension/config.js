3bff80730ce264ea1a0d69636c24bfd754895823
Prakhar Srivastav <prakhar1989@gmail.com>
2014-03-02 19:57:57 +0300

    removed config.js out of version control

--- extension/config.js
+++
@@ -1,6 +0,0 @@
-var POSTMAN_APP_ID_PRODUCTION = "fhbjgbiflinjbdggehcddcbncdddomop";
-var POSTMAN_APP_ID_STAGING = "mgicbpdmnpnfckafakeoiomkjcplfcld";
-var POSTMAN_APP_ID_DEV = "aglpidefogoeiamaehklpfoafichfmdk";
-var POSTMAN_APP_ID_LOCAL = "aglpidefogoeiamaehklpfoafichfmdk";
-
-var postmanAppId = POSTMAN_APP_ID_STAGING;
