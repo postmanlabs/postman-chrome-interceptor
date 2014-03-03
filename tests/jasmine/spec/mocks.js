d8dfd4dcd412a2684799c024ea5b14a718cec31c
Prakhar Srivastav <prakhar1989@gmail.com>
2014-03-03 11:22:43 +0300

    Added mocks for request and a postman message

--- tests/jasmine/spec/mocks.js
+++ tests/jasmine/spec/mocks.js
@@ -1,4 +1,39 @@
 postmanAppId = 'POSTMAN';
+
+var getNewRequest = function() {
+    return {
+        type: 'xmlhttprequest',
+        url: 'localhost:8000',
+        requestId: '1',
+        requestBody: { },
+        requestHeaders: [ ]
+    }
+};
+
+var postmanMessage = function() {
+    return { 
+        "reqId": "10891",
+        "request": {
+            "frameId": 2, "method": "POST",
+            "parentFrameId": 0, "requestBodyType": "formData", //formData or rawData
+            "requestBody": {},
+            "requestId": "10891",
+            "tabId": 161, "timestamp": 1393432789060.789,
+            "type": "xmlhttprequest",
+            "url": "",
+            "requestHeaders": [ ],
+        },
+        "type": "capturedRequest"
+    }
+}
+
+if (!chrome.runtime) {
+    chrome.runtime = {};
+}
+
+chrome.runtime.sendMessage = function() {
+}
+
 chrome.runtime.onConnect = {
 	addListener : function(){}
 };
