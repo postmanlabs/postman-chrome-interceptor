199dc024881fb3fde7506124f55fac1972694418
bihua <realman_1994@outlook.com>
2017-09-08 10:43:08 +0800

    trim console.log

--- extension/js/background_page.js
+++ extension/js/background_page.js
@@ -87,8 +87,6 @@ function getBase64FromArrayBuffer(responseData) {
     var uInt8Array = new Uint8Array(responseData);
     var str = new TextDecoder().decode(uInt8Array);
 
-    console.log(str)
-
     function b64EncodeUnicode(str) {
      return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
         return String.fromCharCode('0x' + p1);
